ea303b6ce3aa90d7f83cdeb5811f8aebd608c541
jgcoding2020 <jgardn11@yahoo.com>
2023-04-24 16:55:39 -0400

    fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; 
+import { UserComponent } from './user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
