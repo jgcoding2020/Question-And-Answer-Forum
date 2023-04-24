5e9c6571730097c7874c3c8a3f260f3337dcc9a5
Joshua David Gardner <76405317+jgcoding2020@users.noreply.github.com>
2023-04-24 16:15:05 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; 
+import { UserComponent } from './user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    
+    id: number = 0;
+    name: string = "";
+    username: string = "";
+    password: string = "";
+    email: string = "";
+    userType: string = "";
+}
