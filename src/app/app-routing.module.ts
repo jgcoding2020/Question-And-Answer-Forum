c9969f7e904ea4abc9eedd98d6d719bc9e261481
jgcoding2020 <jgardn11@yahoo.com>
2023-04-26 10:20:52 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UserLoginComponent } from './login/user-login.component';
+import { AdminLoginComponent } from './login/admin-login.component';
+import { AdminSignUpComponent } from './sign-up/admin-sign-up.component';
+import { CreateQuestionComponent } from './question/create-question.component';
+import { SearchQuestionComponent } from './question/search-question.component';
+import { CreatedQuestionComponent } from './question/created-question.component';
+import { PendingQuestionComponent } from './question/pending-quesiton.component';
+import { CreatedAnswerComponent } from './answer/created-answer.component';
+import { PendingAnswerComponent } from './answer/pending-answer.component';
+import { ApprovedAnswerComponent } from './answer/approved-answer.component';
+import { ChatComponent } from './chat/chat.component';
+import { UserToUserChatComponent } from './chat/user-to-user-chat.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'user-login', component: UserLoginComponent},
+    {path: 'admin-login', component: AdminLoginComponent},
+    {path: 'admin-sign-up', component: AdminSignUpComponent},
+    {path: 'create-question', component: CreateQuestionComponent},
+    {path: 'search-question', component: SearchQuestionComponent},
+    {path: 'created-question', component: CreatedQuestionComponent},
+    {path: 'pending-question', component: AdminSignUpComponent},
+    {path: 'pending-answer', component: PendingAnswerComponent},
+    {path: 'approved-answer', component: ApprovedAnswerComponent},
+    {path: 'chat', component: ChatComponent},
+    {path: 'user-to-user-chat', component: UserToUserChatComponent},
+    {path: '**', component: NoPageFoundComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,28 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'; 
 import { UserComponent } from './user.component';
 
+import { HomeComponent } from './home/home.component';
+import { UserLoginComponent } from './login/user-login.component';
+import { AdminLoginComponent } from './admin-login.component';
+import { AdminSignUpComponent } from './sign-up/admin-sign-up.component';
+import { CreateQuestionComponent } from './question/create-question.component';
+import { SearchQuestionComponent } from './question/search-question.component';
+import { CreatedQuestionComponent } from './question/created-question.component';
+import { PendingQuestionComponent } from './question/pending-quesiton.component';
+import { CreatedAnswerComponent } from './answer/created-answer.component';
+import { PendingAnswerComponent } from './answer/pending-answer.component';
+import { ApprovedAnswerComponent } from './answer/approved-answer.component';
+import { ChatComponent } from './chat/chat.component';
+import { UserToUserChatComponent } from './chat/user-to-user-chat.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent, UserComponent
+    AppComponent, UserComponent, HomeComponent, 
+    UserLoginComponent, AdminLoginComponent, AdminSignUpComponent,
+    CreateQuestionComponent, SearchQuestionComponent, CreatedQuestionComponent,
+    PendingQuestionComponent, CreatedAnswerComponent, PendingAnswerComponent,
+    ApprovedAnswerComponent, ChatComponent, UserToUserChatComponent, NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +38,5 @@ import { UserComponent } from './user.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
