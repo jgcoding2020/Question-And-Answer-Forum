e09d3e76ba7609adaf70f9f7d5fb93b4caf34c42
jdmorrin <morrincomero@outlook.com>
2023-04-26 15:52:33 -0400

    I forgot

--- Project-Draft-Backend/src/main/java/cogent/infotech/com/repositories/UserRepository.java
+++ Project-Draft-Backend/src/main/java/cogent/infotech/com/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package cogent.infotech.com.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import cogent.infotech.com.entities.User;
-
-
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
