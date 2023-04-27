d923b4aae191db5016d15f86ae4a2ee0bdc55c77
jdmorrin <morrincomero@outlook.com>
2023-04-27 09:47:01 -0400

    Added comments

--- Project-Draft-Backend/src/main/java/cogent/infotech/com/repositories/UserRepository.java
+++ Project-Draft-Backend/src/main/java/cogent/infotech/com/repositories/UserRepository.java
@@ -9,6 +9,15 @@ import cogent.infotech.com.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+	// This method is implemented in in CustomUserDetailService when overriding loadByUsername()
+	// Necessary for User authentication
+	// Don't know how it works though
+	// -- Juan David
 	User findByUsername(String username);
+	
+	// I don't know why this is here yet
+	// I'm following a tutorial on JWT and the guy out this method here
+	// Will update if I ever figure it out
+	// -- Juan David
 	Boolean existsByUsername(String username);
 }
