ca337d5c66866b66b36abb7708bf17ec3d8d29b3
Joshua David Gardner <76405317+jgcoding2020@users.noreply.github.com>
2023-04-30 15:58:00 -0400

    Delete src directory

--- src/app/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    
-    id: number = 0;
-    name: string = "";
-    username: string = "";
-    password: string = "";
-    email: string = "";
-    userType: string = "";
-}
